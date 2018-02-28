a13944b5117d81fcef56093eec20eb3288ce574f
gzdeT <36919234+gzdeT@users.noreply.github.com>
2018-02-28 15:18:16 +0300

    topla fonksiyonu

--- GitDenemeApp/GitDenemeApp/denemeClass.cs
+++ GitDenemeApp/GitDenemeApp/denemeClass.cs
@@ -8,5 +8,12 @@ namespace GitDenemeApp
     public class denemeClass
     {
         int i = 0;
+        
+
+       public void Topla()
+        {
+            i = i + 1;
+        }
+        
     }
 }
